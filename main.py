a1eddf6c428f3be322eaad81c02ae9a1991a9563
rip <65096390+b6m@users.noreply.github.com>
2022-07-29 16:27:14 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import httpx, subprocess
+
+class hwid():
+    def __init__(self):
+        self.hwid = self.get_hwid()
+        self.database = 'https://pastebin.com/raw/NmTM1QE5'
+
+    def get_hwid(self):
+        return subprocess.check_output('wmic csproduct get uuid').decode('utf-8').split('\n')[1].strip()
+
+    def check(self):
+        check = httpx.get(self.database)
+        for line in check.text.split('\n'):
+            if 'self.hwid' in line:
+                return True
+        return False
+
+    def main(self):
+        if self.check():
+            print('HWID is in the database')
+        print('HWID is not in the database')
+        exit(1)
+
+if __name__ == '__main__':
+    hwid().main()
